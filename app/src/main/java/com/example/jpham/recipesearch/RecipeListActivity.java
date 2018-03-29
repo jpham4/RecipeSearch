15df698bb6a368d9c900a5f7cb01c4d67dea2fcc
Jimmy Pham <jpham21@outlook.com>
2018-03-29 03:33:15 -0400

    asJsonAsync not working

--- app/src/main/java/com/example/jpham/recipesearch/RecipeListActivity.java
+++ app/src/main/java/com/example/jpham/recipesearch/RecipeListActivity.java
@@ -32,36 +32,14 @@ public class RecipeListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_list);
         lvRecipes = findViewById(R.id.lvRecipes);
-        ArrayList<Recipe> aRecipes = new ArrayList<Recipe>();
+        ArrayList<Recipe> aRecipes = new ArrayList<>();
         recipeAdapter = new RecipeAdapter(this, aRecipes);
         lvRecipes.setAdapter(recipeAdapter);
         fetchRecipes();
     }
 
     private void fetchRecipes() {
-        Future<HttpResponse<JsonNode>> future = Unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?offset=0&diet=vegetarian&excludeIngredients=coconut&intolerances=egg%2C+gluten&number=10&query=burger&type=main+course")
-                .header("accept", "application/json")
-                .header("X-Mashape-Key", "H9O4xN0hDKmshwiRqRSqGj28V9I4p1oEMdYjsn7GuEPdPGOk0q")
-                .header("X-Mashape-Host", "spoonacular-recipe-food-nutrition-v1.p.mashape.com")
-                .asJsonAsync(new Callback<JsonNode>() {
-                    @Override
-                    public void completed(HttpResponse<JsonNode> httpResponse) {
-                        int code = httpResponse.getCode();
-                        Map<String, String> headers = httpResponse.getHeaders();
-                        JsonNode body = httpResponse.getBody();
-                        InputStream rawBody = httpResponse.getRawBody();
-                    }
 
-                    @Override
-                    public void failed(UnirestException e) {
-                        System.out.println("The request has failed");
-                    }
-
-                    @Override
-                    public void cancelled() {
-                        System.out.println("The request has been cancelled");
-                    }
-                });
             }
         }
 
